8ad647f76eff3bf825fb447cc9bf195aedeee8c3
Administrator <Administrator@ASU71>
2019-09-24 18:00:43 +0800

    2019.09.24第一单元

---
+++ ceshihello201909/src/ceshihello/HelloTest.java
@@ -0,0 +1,21 @@
+package ceshihello;
+/**
+ *  这是个测试 
+ * @author Administrator
+ *
+ */
+public class HelloTest {
+
+	/**
+	 * 
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello java");
+
+	}
+
+}
---
+++ jisuanyuanmianjie201909/src/cn/edu/asu/mianji/JSMJ.java
@@ -0,0 +1,14 @@
+/**
+ * 包名:唯一限定这个类。
+ * 
+ * 怎么命名包名：利用我们网站的域名的反写来限定,以"."进行分割，包名必须是小写
+ * 	URL:http://www.asu.edu.cn/
+ * 域名：www.asu.edu.cn
+ */
+package cn.edu.asu.mianji;
+
+
+//定义一个类
+public class JSMJ {
+
+}
